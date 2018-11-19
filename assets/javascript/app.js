6d49f356065a2b2711c9738449b32d947f507b30
Zach Merel <zachmerel@gmail.com>
2018-11-19 14:45:28 -0600

    saving to get back to master branch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,6 +20,24 @@ googleSignIn=()=>{
         console.log("Failed to connect");
     })
 }
+//function to render google sign in button
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  }
+  function onFailure(error) {
+    console.log(error);
+  }
+function renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': onSuccess,
+      'onfailure': onFailure
+    });
+  }
 
 //Hide carousel, results, and about us divs by default
 $("#celebCarouselBody").hide();
